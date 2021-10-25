bf27cfcd247156c70d14b8df50b756f6f6d463bd
distortedroom <smolnik@59.ru>
2021-10-25 13:46:46 +0300

    Second commit Upgraded todoist, added REST files

--- src/main/java/com/example/telebot/TaskController.java
+++ src/main/java/com/example/telebot/TaskController.java
@@ -6,23 +6,23 @@ import java.util.List;
 
 @RestController
 public class TaskController {
-    @GetMapping(value = "/tasks", produces = "application/json")
-    List<Task> allTasks(){
-
-    }
-
-    @PostMapping(value = "/tasks", consumes = "application/json", produces = "application/json")
-    Task newTasks(){
-
-    }
-
-    @PutMapping(value = "/tasks/{id}", consumes = "application/json", produces = "application/json")
-    Task updateTask(@PathVariable String id){
-
-    }
-
-    @DeleteMapping(value = "/tasks/{id}")
-    void deleteTask(@PathVariable String id) {
-
-    }
+//    @GetMapping(value = "/tasks", produces = "application/json")
+//    List<Task> allTasks(){
+//
+//    }
+//
+//    @PostMapping(value = "/tasks", consumes = "application/json", produces = "application/json")
+//    Task newTasks(){
+//
+//    }
+//
+//    @PutMapping(value = "/tasks/{id}", consumes = "application/json", produces = "application/json")
+//    Task updateTask(@PathVariable String id){
+//
+//    }
+//
+//    @DeleteMapping(value = "/tasks/{id}")
+//    void deleteTask(@PathVariable String id) {
+//
+//    }s
 }
