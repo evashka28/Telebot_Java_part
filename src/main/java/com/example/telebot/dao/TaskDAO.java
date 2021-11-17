df258c354ec4e2bcffe1221f1924bd1e4cdc3387
distortedroom <nikitos1151@gmail.com>
2021-11-17 14:09:44 +0300

    Twelfth commit Criteria to hql

--- src/main/java/com/example/telebot/dao/TaskDAO.java
+++ src/main/java/com/example/telebot/dao/TaskDAO.java
@@ -1,6 +1,7 @@
 package com.example.telebot.dao;
 
 import com.example.telebot.Project;
+import com.example.telebot.Tag;
 import com.example.telebot.Task;
 import com.example.telebot.utils.HibernateSessionFactoryUtil;
 import org.hibernate.Criteria;
@@ -27,15 +28,11 @@ public class TaskDAO extends AbstractDAO<Task>{
 
     public List<Task> getAllByProjectId(long projectId, boolean favourite){
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<Task> criteriaQuery = criteriaBuilder.createQuery(Task.class);
-        Root<Task> root = criteriaQuery.from(Task.class);
-
-        criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("projectId"), projectId));
-
-        Query<Task> query = session.createQuery(criteriaQuery);
-
-        List<Task> output = query.getResultList();
+        Query query = session.createQuery("from Task where projectId = :projectIdParam " +
+                "and favourite = :favouriteParam");
+        query.setParameter("projectIdParam", projectId);
+        query.setParameter("favouriteParam", favourite);
+        List<Task> output = (List<Task>) query.getResultList();
 
         session.close();
         return output;
@@ -47,16 +44,15 @@ public class TaskDAO extends AbstractDAO<Task>{
 
     public Task getWithOldestLastAccessByProjectId(long projectId){
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<Task> criteriaQuery = criteriaBuilder.createQuery(Task.class);
-        Root<Task> root = criteriaQuery.from(Task.class);
-
-        List<Predicate> predicates = new ArrayList<Predicate>();
-        predicates.add(criteriaBuilder.equal(root.get("projectId"), projectId));;
-
-        Query<Task> query = session.createQuery(criteriaQuery);
-
-        Task output = query.getSingleResult();
+        Query query = session.createQuery("from Task where projectId = :projectIdParam " +
+                "and favourite = :favouriteParam " +
+                "and lastAccessDatetime = " +
+                "(SELECT min(lastAccessDatetime) from Task where projectId = :projectIdParam " +
+                "and favourite = :favouriteParam)");
+        query.setParameter("projectIdParam", projectId);
+        query.setParameter("favouriteParam", false);
+        query.setMaxResults(1);
+        Task output = (Task) query.getSingleResult();
 
         session.close();
 
